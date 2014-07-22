431b320b57709cd16998f8f337e6bd95fb40e2ed
Robert Forgione <rforgione@gmail.com>
2014-07-22 03:23:25 -0400

    Condensed gsub expressions used to make more legible column labels

---  run_analysis.R
+++  run_analysis.R
@@ -17,40 +17,6 @@ run_analysis <- function() {
     # Use descriptive activity names to name the activities in the data set
     features <- features[relcol]
     
-#     features <- gsub("tBodyAccJerk", "timeSignalAccelerometerJerk", features)
-#     features <- gsub("tBodyAccMag", "timeSignalBodyAccelerometerMagnitude", features)
-#     features <- gsub("tBodyAcc", "timeSignalBodyAccelerometer", features)
-#     
-#     features <- gsub("tGravityAccMag", "timeSignalGravityAccelerometerMagnitude", features)
-#     features <- gsub("tGravityAcc", "timeSignalGravityAccelerometer", features)
-#     
-#     features <- gsub("tBodyGyroJerkMag", "timeSignalGravityGyroscopeJerkMagnitude", features)
-#     features <- gsub("tBodyGyroJerk", "timeSignalBodyGyroscopeJerk", features)
-#     features <- gsub("tBodyGyroMag", "timeSignalGravityGyroscopeMagnitude", features)
-#     features <- gsub("tBodyGyro", "timeSignalBodyGyroscope", features)
-#     
-#     features <- gsub("fBodyAccJerk", "fourierBodyAccelerometerJerk", features)
-#     features <- gsub("fBodyAccMag", "fourierBodyAccelerometerMagnitude", features)
-#     features <- gsub("fBodyAcc", "fourierBodyAccelerometer", features)
-#     
-#     features <- gsub("fBodyGyroJerkMag", "fourierBodyGyroscopeJerkMagnitude", features)
-#     features <- gsub("fBodyGyroMag", "fourierBodyGyroscopeMagnitude", features)
-#     features <- gsub("fBodyGyro", "fourierBodyGyroscope", features)
-#     
-#     features <- gsub("fBodyAccJerkMag", "fourierBodyAccelerometerJerkMagnitude", features)
-#     
-#     features <- gsub("fBodyBodyAccJerkMag", "fourierBodyBodyAccelerometerJerkMagnitude", features)
-#     features <- gsub("fBodyBodyGyroAccJerkMag", "fourierBodyBodyGyroscopeAccelerometerJerkMagnitude", features)
-#     features <- gsub("fBodyBodyGyroMag", "fourierBodyBodyGyroscopeMagnitude", features)
-#     features <- gsub("fBodyBodyGyroJerkMag", "fourierBodyBodyGyroscopeJerkMagnitude", features)
-#     
-#     features <- gsub("-mean\\(\\)", "Mean", features)
-#     features <- gsub("-std\\(\\)", "StandardDeviation", features)
-#     
-#     features <- gsub("-X", "XAxis", features)
-#     features <- gsub("-Y", "YAxis", features)
-#     features <- gsub("-Z", "ZAxis", features)
-    
     features <- gsub("^t", "timeSignal", features)
     features <- gsub("^f", "fourierSignal", features)
     features <- gsub("Acc", "Accelerometer", features)
