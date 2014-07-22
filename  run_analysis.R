9e939e3938b6b5367961121ee17295ceecbf73b3
Robert Forgione <rforgione@gmail.com>
2014-07-22 16:40:19 -0400

    Completed final piece of assignment

---  run_analysis.R
+++  run_analysis.R
@@ -30,10 +30,33 @@ run_analysis <- function() {
     features <- gsub("-Y", "YAxis", features)
     features <- gsub("-Z", "ZAxis", features)
     
-    features
-    
     # Appropriately label the data set with descriptive variable names
     
+    colnames(reduced_set) <- features
     
     # Create a second, independent tidy data set with the average of each variable for each activity and each subject
+    
+    train_users <- read.table("data/train/subject_train.txt")
+    test_users <- read.table("data/test/subject_test.txt")
+    
+    users <- rbind(train_users, test_users)
+    colnames(users) <- "User"
+    
+    reduced_set <- cbind(users, reduced_set)
+    reduced_set <- reduced_set[order(reduced_set$User),]
+    
+    new_data_cols <- dim(reduced_set)[2]
+    new_data_rows <- length(unique(reduced_set$User))
+    
+    new_data <- matrix(nrow=new_data_rows, ncol=new_data_cols)
+    new_data <- as.data.frame(new_data)
+    
+    for (i in 1:length(new_data)) {
+        new_data[[i]] <- tapply(reduced_set[[i]], reduced_set$User, mean)
+    }
+    
+    colnames(new_data) <- colnames(reduced_set)
+    
+    print(colnames(new_data))
+    dim(new_data)
 }
